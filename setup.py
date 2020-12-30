8710f7b58d995759ab7afabdd56400973ffdab56
Naeem Ilyas <naeem-ilyas@live.com>
2020-12-30 21:24:51 +0500

    MCKIN-28363 - Fixes import and exceptions

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def package_data(pkg, roots):
 
 setup(
     name='scormxblock-xblock',
-    version='3.2.1',
+    version='3.2.2',
     description='XBlock to integrate SCORM content packages',
     packages=[
         'scormxblock',
